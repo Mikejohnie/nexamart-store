b55f6a62ba02c77ec6e7d182be3cfad24db6baca
michael <nkumichael1@gmail.com>
2025-12-13 23:04:39 +0100

    initial commit

--- src/hooks/useDashboardEvents.ts
+++ src/hooks/useDashboardEvents.ts
@@ -1,6 +1,6 @@
 "use client";
 
-//import { pusherClient } from "@/lib/pusher"; fix any here
+import { pusherClient } from "@/lib/pusher";
 import { useEffect } from "react";
 import { toast } from "sonner";
 
